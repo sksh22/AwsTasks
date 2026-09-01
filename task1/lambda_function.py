491c7b1b7c33f30615fec54330b9f6800e635c80
Sakshi Labde <labdes38@gmail.com>
2026-09-02 00:32:53 +0530

    Remove unnecessary line breaks in lambda_function.py

--- task1/lambda_function.py
+++ task1/lambda_function.py
@@ -9,9 +9,7 @@ def lambda_handler(event, context):
     source_bucket = event["Records"][0]["s3"]["bucket"]["name"]
 
     # Get uploaded file name
-    object_key = urllib.parse.unquote_plus(                               
-        event["Records"][0]["s3"]["object"]["key"]
-    )
+    object_key = urllib.parse.unquote_plus(event["Records"][0]["s3"]["object"]["key"])
 
     # Get destination bucket
     destination_bucket = "sksh-destinationbucket-2026"
